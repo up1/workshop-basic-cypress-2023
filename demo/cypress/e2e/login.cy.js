89e28be0ee9c213108ab91b52d722d8fb22d9627
up1 <somkiat.p@gmail.com>
2023-06-18 22:18:31 +0700

    Workshop 01

--- demo/cypress/e2e/login.cy.js
+++ demo/cypress/e2e/login.cy.js
@@ -1,5 +1,18 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    // Arrange
+    cy.visit('https://demo-login-workshop.vercel.app')
+    
+    // Act
+    cy.get('#username_field').should('be.visible').type('demo')
+    cy.get('#password_field').should('be.visible').type('mode')
+    cy.get('#login_button').should('be.visible').click()
+
+    // Assert
+    cy.contains('Login succeeded. Now you can logout.')
+      .should('be.visible')
+
+    cy.get('#container > h1').contains('Welcome Page').should('be.visible')
+    cy.get('#container > p').contains('Login succeeded. Now you can logout.').should('be.visible')
   })
 })
