9584592f4a6710c8a4342639d9c981d6eebf8e16
up1 <somkiat.p@gmail.com>
2023-06-19 09:58:59 +0700

    Demo day 1 with cypress

---
+++ day01/cypress/e2e/hello.cy.js
@@ -0,0 +1,15 @@
+describe('template spec', () => {
+
+  it('passes 01', () => {
+    cy.visit('https://www.google.com')
+  })
+  
+  it('passes 02', () => {
+    cy.visit('https://www.google.com')
+  })
+  
+  it('passes 03', () => {
+    cy.visit('https://www.google.com')
+  })
+
+})
