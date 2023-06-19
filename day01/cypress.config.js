52e4750a2f6c1ebafbb997aef2a40091f8c33a1c
up1 <somkiat.p@gmail.com>
2023-06-19 10:14:51 +0700

    Use baseUrl

--- day01/cypress.config.js
+++ day01/cypress.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   e2e: {
+    baseUrl: 'https://www.google.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- day01/cypress/e2e/hello.cy.js
+++ day01/cypress/e2e/hello.cy.js
@@ -1,15 +1,15 @@
 describe('template spec', () => {
 
   it('passes 01', () => {
-    cy.visit('https://www.google.com')
+    cy.visit('/')
   })
   
   it('passes 02', () => {
-    cy.visit('https://www.google.com')
+    cy.visit('/')
   })
   
   it('passes 03', () => {
-    cy.visit('https://www.google.com')
+    cy.visit('/')
   })
 
 })
