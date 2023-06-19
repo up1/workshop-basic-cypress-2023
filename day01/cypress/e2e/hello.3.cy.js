973ed6834334a187c3bca8b435df71c4045cd716
up1 <somkiat.p@gmail.com>
2023-06-19 14:26:30 +0700

    Data driven testing

--- day01/cypress/e2e/hello.3.cy.js
+++ day01/cypress/e2e/hello.3.cy.js
@@ -1,15 +1,19 @@
-import search from '../fixtures/search.json'
+import datas from '../fixtures/search.json'
 describe('Search with google.com', () => {
 
-  it('Success with search by keyword=cypress', () => {
-    cy.visit('/')
-    // Act
-    cy.get('[name="q"]').type(search.keyword)
-    cy.get('[name="q"]').type('{enter}')
-    // Assert
-    cy.get('#result-stats').should('be.visible')
-    cy.get('#result-stats').contains('ผลการค้นหาประมาณ')
-    cy.get('#result-stats').should('include.text', 'ผลการค้นหาประมาณ')
+  datas.forEach( data => {
+    it(data.case_name, () => {
+      cy.visit('/')
+      // Act
+      cy.get('[name="q"]').type(data.keyword)
+      cy.get('[name="q"]').type('{enter}')
+      // Assert
+      cy.get('#result-stats').should('be.visible')
+      cy.get('#result-stats').contains('ผลการค้นหาประมาณ')
+      cy.get('#result-stats').should('include.text', 'ผลการค้นหาประมาณ')
+    })
   })
+
+  
   
 })
