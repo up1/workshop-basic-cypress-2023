fde545613e93837071e04a03ab440d580026a30a
up1 <somkiat.p@gmail.com>
2023-06-19 11:47:51 +0700

    Network request + intercept + wait

---
+++ day01/cypress/e2e/hello2.cy.js
@@ -0,0 +1,26 @@
+describe('Search with google.com', () => {
+  beforeEach(()=> {
+    cy.intercept(
+      {
+        method: 'GET', 
+        url: '/complete/search*', 
+      },
+      [] 
+    ).as('search')
+    cy.intercept(
+      {
+        method: 'GET', 
+        url: '/client_204*', 
+      },
+      [] 
+    ).as('client_204')
+    cy.visit('/')
+    cy.wait(['@search', '@client_204'])
+  })
+
+  it('Success with load first page', () => { 
+    cy.get('#APjFqb').should('be.visible')
+    cy.get('[name="q"]').should('be.visible')
+  })
+
+})
