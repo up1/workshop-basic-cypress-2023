f14e5f32b3e8861a1ccd47201a8659580a2e8ef4
up1 <somkiat.p@gmail.com>
2023-06-19 13:54:25 +0700

    Add tags

--- day01/cypress/e2e/hello2.cy.js
+++ day01/cypress/e2e/hello2.cy.js
@@ -1,24 +1,24 @@
 describe('Search with google.com', () => {
-  beforeEach(()=> {
+  beforeEach(() => {
     cy.intercept(
       {
-        method: 'GET', 
-        url: '/complete/search*', 
+        method: 'GET',
+        url: '/complete/search*',
       },
-      [] 
+      []
     ).as('search')
     cy.intercept(
       {
-        method: 'GET', 
-        url: '/client_204*', 
+        method: 'GET',
+        url: '/client_204*',
       },
-      [] 
+      []
     ).as('client_204')
     cy.visit('/')
     cy.wait(['@search', '@client_204'])
   })
 
-  it('Success with load first page', {tags: ['group01']}, () => { 
+  it('Success with load first page', { tags: ['group01'] }, () => {
     cy.get('#APjFqb').should('be.visible')
     cy.get('[name="q"]').should('be.visible')
   })
