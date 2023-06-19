555ebb3336146d94dfefcfaac777fa09ae2ed0bc
up1 <somkiat.p@gmail.com>
2023-06-19 11:19:54 +0700

    Action with test case

--- day01/cypress/e2e/hello.cy.js
+++ day01/cypress/e2e/hello.cy.js
@@ -1,10 +1,21 @@
 describe('template spec', () => {
 
-  it('passes 01', () => {
+  it('Success with load first page', () => {
     cy.visit('/')
     cy.get('#APjFqb').should('be.visible')
     cy.get('[name="q"]').should('be.visible')
   })
-  
 
+  it('Success with search by keyword=cypress', () => {
+    // Arrange
+    cy.visit('/')
+    // Act
+    cy.get('[name="q"]').type('cypress')
+    cy.get('[name="q"]').type('{enter}')
+    // Assert
+    cy.get('#result-stats').should('be.visible')
+    cy.get('#result-stats').contains('ผลการค้นหาประมาณ')
+    cy.get('#result-stats').should('include.text', 'ผลการค้นหาประมาณ')
+  })
+  
 })
